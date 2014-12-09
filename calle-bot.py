df6d6b6b9c1820b9f078042e418cacc8a39b32b5
Dexter Gramfors <dexter.gramfors@gmail.com>
2014-12-09 20:12:40 +0100

    Added bigram and trigram construction, as well as a larger tweet test file.

--- calle-bot.py
+++ calle-bot.py
@@ -9,7 +9,7 @@ CONSUMER_KEY = 'cfXIieakwAlNGRq1x60oVb05c'
 CONSUMER_SECRET = 'sIzLofT62RNpZEKDcc6cRwZFtkiiyngKzXBgyNxutfE8LIWszm'
 ACCESS_KEY = '2648673991-BJyAcIKs9XJp9CNjDWsnlEYSGpb0Yeno2LHdzss'
 ACCESS_SECRET = 'p4xtAW31nvVtLZ1quvO5pjoR9R09MG3StoZDcfwQagnd2'
-NUMBER_OF_TWEETS = 300
+NUMBER_OF_TWEETS = 2000
 fake_tweets = ["I don't even know", "Dark lord cthulhu", "I love moderaterna, they are awesome", "http://wat.com", "lol, why are we doing this"]
 #setup stuff
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -17,7 +17,7 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 def write_test_file(tweets):
-	with open('test_tweets', 'r+a') as f:
+	with open('test_tweets_2', 'w') as f:
 		for tweet in tweets:
 			f.write(tweet.strip('\n').encode('utf8'))
 			f.write('\n')
@@ -42,23 +42,34 @@ def get_words(tweets):
 
 def generate_test_file_from_tweets():
 	tweets = get_filtered_tweets()
+	print len (tweets)
 	write_test_file(tweets)
 
 def get_file_tweets():
-	with open('test_tweets', 'r') as f:
+	with open('test_tweets_2', 'r') as f:
 		tweets = f.readlines()
 	return tweets
 
 def get_file_wordlists():
 	tweets = get_file_tweets()
-	worldlists = get_words(tweets)
-	return worldlists
+	wordlists = get_words(tweets)
+	return wordlists
 
 def get_twitter_wordlists():
 	tweets = get_filtered_tweets()
-	worldlists = get_words(tweets)
-	return worldlists
+	wordlists = get_words(tweets)
+	return wordlists
+
+def get_bigrams(wordlists):
+	return zip(zip(wordlists, wordlists[1:]), wordlists[2:])
+
+def get_trigrams(wordlists):
+	return zip(zip(wordlists, wordlists[1:], wordlists[2:]), wordlists[3:])
 
 if __name__ == '__main__':
-	worldlists = get_file_wordlists()
-	print worldlists
+	tweets = get_file_tweets()
+	print tweets
+	print len(tweets)
+	wordlists = get_file_wordlists()
+	bigrams = get_bigrams(wordlists[0])
+	print bigrams
